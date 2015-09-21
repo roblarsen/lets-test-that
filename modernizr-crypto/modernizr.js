4869614c60298bfaa41fdc220b254b8f82e26f22
Rob Larsen <rob@htmlcssjavascript.com>
2015-09-20 21:41:56 -0400

    fun times

--- modernizr-crypto/modernizr.js
+++ modernizr-crypto/modernizr.js
@@ -1088,14 +1088,14 @@
   
 /*!
 {
-  "name": "getRandomValues",
-  "property": "getrandomvalues",
-  "caniuse": "window.crypto.getRandomValues",
+  "name": "Web Cryptography",
+  "property": "cryptography",
+  "caniuse": "cryptography",
   "tags": ["crypto"],
-  "authors": ["komachi"],
+  "authors": ["roblarsen"],
   "notes": [{
-    "name": "W3C Editor’s Draft",
-    "href": "https://dvcs.w3.org/hg/webcrypto-api/raw-file/tip/spec/Overview.html#RandomSource-method-getRandomValues"
+    "name": "W3C Editorâs Draft",
+    "href": "http://www.w3.org/TR/WebCryptoAPI/"
   }],
   "polyfills": [
     "polycrypt"
@@ -1103,23 +1103,18 @@
 }
 !*/
 /* DOC
-Detects support for the window.crypto.getRandomValues for generate cryptographically secure random numbers
+Detects support for the cryptographic functionality available under window.crypto.subtle
 */
 
-  // In Safari <=5.0 `window.crypto` exists (for some reason) but is `undefined`, so we have to check
-  // it’s truthy before checking for existence of `getRandomValues`
   var crypto = prefixed('crypto', window);
-  var supportsGetRandomValues;
+  var hasSubtle = false;
 
-  // Safari 6.0 supports crypto.getRandomValues, but does not return the array,
-  // which is required by the spec, so we need to actually check.
-  if (crypto && 'getRandomValues' in crypto && 'Uint32Array' in window) {
-    var array = new Uint32Array(10);
-    var values = crypto.getRandomValues(array);
-    supportsGetRandomValues = values && is(values[0], 'number');
+  if (crypto && 'subtle' in crypto) {
+    hasSubtle = true;
   }
 
-  Modernizr.addTest('getrandomvalues', !!supportsGetRandomValues);
+  Modernizr.addTest('cryptography', hasSubtle);
+
 
 
   // Run each test
